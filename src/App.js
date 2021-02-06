93f7992a2608efdedde5dcfeb0a3ef14376686e5
itspanicky <nicky.li92@gmail.com>
2021-02-05 21:00:44 -0500

    feat: fetch user id from local storage and save to state

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import './App.css'
 import Home from './pages/Home'
 import Lobby from './pages/Lobby'
+import { useStateValue } from './StateProvider'
+import { actionTypes } from './reducer'
+import { playerId } from './services/localStorage'
 
 function App() {
+  const [{ user }, dispatch] = useStateValue()
+
+  useEffect(() => {
+    // fetch user id from local storage and save to state
+    if (!user) {
+      const _user = playerId()
+      console.log("user", _user)
+      dispatch({
+        type: actionTypes.SET_USER,
+        user: _user,
+      })
+    }
+  }, [user, dispatch])
+
   return (
     <div className="App">
       <Router>

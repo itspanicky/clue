66a2bf592dfc520fda336313836f59bee6532b92
itspanicky <nicky.li92@gmail.com>
2021-02-12 21:26:05 -0500

    refactor: firebase create room attributes

--- src/components/EnterRoom/index.js
+++ src/components/EnterRoom/index.js
@@ -1,83 +1,93 @@
-import React, { useState, useEffect } from 'react'
-import './index.css'
-import { Button, TextField } from '@material-ui/core'
-import db from '../../firebase'
-import { nanoid } from 'nanoid'
-import { useHistory } from 'react-router-dom'
-import { useStateValue } from '../../StateProvider'
-import firebase from 'firebase'
+import React, { useState, useEffect } from "react";
+import "./index.css";
+import { Button, TextField } from "@material-ui/core";
+import db from "../../firebase";
+import { nanoid } from "nanoid";
+import { useHistory } from "react-router-dom";
+import { useStateValue } from "../../StateProvider";
+import firebase from "firebase";
 
 // Create a new room or join an existing room
 function EnterRoom() {
-  const history = useHistory()
+  const history = useHistory();
   const [{ user }] = useStateValue();
 
-  const [toggle, setToggle] = useState(false) // toggle input for joining room
-  const [roomCode, setRoomCode] = useState(null)
-  const [invalidCode, setInvalidCode] = useState(null)
+  const [toggle, setToggle] = useState(false); // toggle input for joining room
+  const [roomCode, setRoomCode] = useState(null);
+  const [invalidCode, setInvalidCode] = useState(null);
 
   useEffect(() => {
     // clear invalid room code when text field input changes
-    setInvalidCode(false)
-  }, [roomCode])
+    setInvalidCode(false);
+  }, [roomCode]);
 
   const createRoom = () => {
     // create a new room with a randomized 5 length string room code and reroute to new room
 
-    const newRoomCode = nanoid(5).toUpperCase()
-    db.collection('rooms').doc(newRoomCode).set({
+    const newRoomCode = nanoid(5).toUpperCase();
+    db.collection("rooms").doc(newRoomCode).set({
       host: user,
-      players: [],  // { playerId, playerName, role }
-      status: 'waiting',  // waiting, playing
-      timestamp: firebase.firestore.FieldValue.serverTimestamp()
-    })
-    history.push(`/${newRoomCode}/Lobby`)
-  }
-  
+      status: "waiting", // waiting, playing
+      messages: [], // { user, message }
+      timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+    });
+    history.push(`/${newRoomCode}/Lobby`);
+  };
+
   const joinRoom = () => {
     // redirect to room if room exist
-    db.collection('rooms').doc(roomCode)
-      .get().then((doc) => {
+    db.collection("rooms")
+      .doc(roomCode)
+      .get()
+      .then((doc) => {
         debugger;
         if (doc.exists) {
-          history.push(`/${roomCode}/Lobby`)
+          history.push(`/${roomCode}/Lobby`);
         } else {
-          setInvalidCode(true)
+          setInvalidCode(true);
         }
-      })
-  }
+      });
+  };
 
   return toggle ? (
-    <div className='enterRoom'>
+    <div className="enterRoom">
       <TextField
         error={invalidCode}
         autoFocus
-        label={invalidCode ? 'Room does not exist' : 'Room Code'}
-        variant='filled' 
-        size='small' 
+        label={invalidCode ? "Room does not exist" : "Room Code"}
+        variant="filled"
+        size="small"
         value={roomCode}
         onChange={({ target: { value } }) => setRoomCode(value.toUpperCase())}
-        inputProps={{ 
-          maxLength: 5, 
-          style: { background: '#f8f8f8', textTransform: 'uppercase' } }}
+        inputProps={{
+          maxLength: 5,
+          style: { background: "#f8f8f8", textTransform: "uppercase" },
+        }}
       />
-        <Button onClick={joinRoom} className='enterRoom__joinButton' disabled={!roomCode}>
-          Join Room
-        </Button>
-        <Button onClick={() => setToggle(false)} className='enterRoom__backButton'>
-          Back
-        </Button>
+      <Button
+        onClick={joinRoom}
+        className="enterRoom__joinButton"
+        disabled={!roomCode}
+      >
+        Join Room
+      </Button>
+      <Button
+        onClick={() => setToggle(false)}
+        className="enterRoom__backButton"
+      >
+        Back
+      </Button>
     </div>
   ) : (
-    <div className='enterRoom'>
-      <Button onClick={createRoom} className='enterRoom__createButton'>
+    <div className="enterRoom">
+      <Button onClick={createRoom} className="enterRoom__createButton">
         Create Room
       </Button>
-      <Button onClick={() => setToggle(true)} className='enterRoom__joinButton'>
+      <Button onClick={() => setToggle(true)} className="enterRoom__joinButton">
         Join Room
       </Button>
     </div>
-  )
+  );
 }
 
-export default EnterRoom
+export default EnterRoom;

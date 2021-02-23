3d6bf8dbbbb6f373596caf7f964ba73670ce7cec
itspanicky <nicky.li92@gmail.com>
2021-02-23 18:53:10 -0500

    refactor: eslint formatting

--- src/App.js
+++ src/App.js
@@ -1,39 +1,41 @@
-import React, { useEffect } from 'react'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import './App.css'
-import Home from './pages/Home'
-import Lobby from './pages/Lobby'
-import Header from './components/Header'
-import { useStateValue } from './StateProvider'
-import { actionTypes } from './reducer'
-import { playerId } from './services/localStorage'
+import React, { useEffect } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import "./App.css";
+import Home from "./pages/Home";
+import Lobby from "./pages/Lobby";
+import Game from "./pages/Game";
+import Header from "./components/Header";
+import { useStateValue } from "./StateProvider";
+import { actionTypes } from "./reducer";
+import { playerId } from "./services/localStorage";
 
 function App() {
-  const [{ user }, dispatch] = useStateValue()
+  const [{ user }, dispatch] = useStateValue();
 
   useEffect(() => {
     // fetch user id from local storage and save to state
     if (!user) {
-      const _user = playerId()
-      console.log("user", _user)
+      const _user = playerId();
+      console.log("user", _user);
       dispatch({
         type: actionTypes.SET_USER,
         user: _user,
-      })
+      });
     }
-  }, [user, dispatch])
+  }, [user, dispatch]);
 
   return (
     <div className="App">
       <Header />
       <Router>
         <Switch>
-          <Route path='/:roomCode/Lobby' component={Lobby} />
-          <Route path='/' component={Home} />
+          <Route path="/:roomCode/Game" component={Game} />
+          <Route path="/:roomCode/Lobby" component={Lobby} />
+          <Route path="/" component={Home} />
         </Switch>
       </Router>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
